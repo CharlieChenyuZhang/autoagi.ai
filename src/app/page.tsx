80ec596470d7b779db17e0a331dd5ecfef9cf0f8
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2023-05-29 14:54:06 -0400

    change the wordings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,13 +28,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            AI+Web3{" "}
+            AI+Education{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Answer any Web3 questions through natural language.
+            We are on the verge of a major shift in education and AutoAGI.AI is
+            leading the way.
           </p>
         </a>
 
